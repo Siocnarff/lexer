23e78c5696a6d28541e949480558de44b27bc0dd
Josua Botha <josuabotha@protonmail.ch>
2021-04-08 08:18:32 +0200

    add real buffer, no longer lose input when no space is employed

--- lexer.rb
+++ lexer.rb
@@ -53,11 +53,12 @@ end
 class DFA
 	def initialize(dfa)
 		@buffer = ''
+		@token = ''
 		@current_state = 1
 		@states = Array.new
 		dfa['states'].each do |s|
-			alpha_ex_go_to = -1
-			do_alpha_ex = false
+			alpha_ex_go_to = 0
+			do_alpha_numa_ex = false
 			s_id = s.keys[0]
 			s_data = s.values[0]
 			state = State.new(s_id, s_data["label"], s_data["error"])
@@ -67,17 +68,19 @@ class DFA
 					if t[0].is_a?(Array)
 						letters += t[0]
 						state.add_transition(Transition.new(t[0], t[1]))
-					elsif t[0] == 'alpha_ex'
-						do_alpha_ex = true
+					elsif t[0] == 'alpha_numa_ex'
+						do_alpha_numa_ex = true
 						alpha_ex_go_to = t[1]
 					else
 						letters += dfa[t[0]]
 						state.add_transition(Transition.new(dfa[t[0]], t[1]))
 					end
 				end
-				if do_alpha_ex
+				if do_alpha_numa_ex
 					state.add_transition(
-						Transition.new(dfa['alpha'] - letters, alpha_ex_go_to)
+						Transition.new(
+							(dfa['alpha'] | dfa['dnull']) - letters, alpha_ex_go_to
+						)
 					)
 				end
 			end
@@ -85,28 +88,36 @@ class DFA
 		end
 	end
 	def reset
-		@buffer = ''
+		@token = ''
 		@current_state = 1
 	end
 	def eat(k)
+		@buffer = ''
 		current = @states[@current_state]
-		if k.ord == 10
+		if k.ord == 10 or k.ord == 13
 			return false
-		end
-		if k.ord == 32 and current.is_accepting?
-			return [current.get_class_label, @buffer]
+		elsif k.ord == 32 and current.is_accepting?
+			return [current.get_class_label, @token]
+		else
+			@buffer = k
 		end
 		begin
 			@current_state = current.eat(k)
 		rescue
 			if current.is_accepting?
-				return [current.get_class_label, @buffer]
+				return [current.get_class_label, @token]
 			else
 				raise current.get_error
 			end
 		end
-		@buffer += k
+		@token += k
 		return false
+	end
+	def token
+		return @token
+	end
+	def data_in_buffer?
+		return @buffer != ''
 	end
 	def buffer
 		return @buffer
@@ -122,20 +133,26 @@ file_data = file.read
 file.close
 
 count = 1
-file_data.split('').each do |k|
+file_data.split('').push(' ').each do |k|
 	begin
 		info = dfa.eat(k)
 		if info
 			puts "#{count} #{info[0]} #{info[1]}"
 			dfa.reset
+			if dfa.data_in_buffer?
+				dfa.eat(dfa.buffer)
+			end
 			count += 1
 		end
 	rescue Exception => e
 		puts "\n======================================="
-		puts "    ERROR at text: #{dfa.buffer}"
+		puts "    ERROR at text: #{dfa.token}"
 		puts "    #{e}"
 		puts "    #{k} is illegal here."
 		puts "=======================================\n"
 		break
 	end
 end
+
+
+#todo: fix error where we lose valid input if no space is used : do real buffer
